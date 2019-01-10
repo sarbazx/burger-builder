e166bf56f55516ef54238be592e5b9f506d91f3c
Taskhyn Maksim <taskin@drupart.net>
2019-01-10 14:19:56 +0300

    flattern ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import styles from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
       })
-    });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, [])
+
+  if (transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
 
   return (
     <div className={styles.Burger}>
