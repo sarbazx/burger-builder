a6626eccbc095429eb0a07f29b6d9bda24d2407e
Taskhyn Maksim <tasqynm@gmail.com>
2019-01-15 20:40:43 +0300

    implemented checkout modal

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -24,7 +24,11 @@ const buildControls = props => {
           disabled={props.disabled[ctrl.type]}
         />
       ))}
-      <button className={styles.OrderButton} disabled={!props.purchasable}>
+      <button
+        className={styles.OrderButton}
+        disabled={!props.purchasable}
+        onClick={props.ordered}
+      >
         ORDER NOW
       </button>
     </div>
