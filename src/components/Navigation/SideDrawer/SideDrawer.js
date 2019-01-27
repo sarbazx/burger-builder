3199b4fe15f54a806ab21345a8c5cad3cde4728d
Taskhyn Maksim <tasqynm@gmail.com>
2019-01-27 14:55:43 +0300

    responsive-adjustments

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,7 +6,9 @@ import styles from "./SideDrawer.module.css";
 const sideDrawer = props => {
   return (
     <div className={styles.SideDrawer}>
-      <Logo />
+      <div className={styles.Logo}>
+        <Logo />
+      </div>
       <nav>
         <NavigationItems />
       </nav>
