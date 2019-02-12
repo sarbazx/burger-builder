700f7cfb18458c2a38f5e701f4bdc527e1e654c9
Taskhyn Maksim <tasqynm@gmail.com>
2019-02-12 17:51:07 +0300

    add hamburger icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,12 @@
 import React from "react";
+import styles from "./DrawserToggle.module.css";
 
-const drawerToggle = props => <div onClick={props.clicked}>MENU</div>;
+const drawerToggle = props => (
+  <div class={styles.DrawerToggle} onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
 
 export default drawerToggle;
