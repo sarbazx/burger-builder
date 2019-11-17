08bbb2e81a8c5a3c5911c9962cc55cbb5e21c97e
Taskhyn Maksim <tasqynm@gmail.com>
2019-11-17 16:53:20 +0300

    dynamically render burger ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 export default class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <div>
         <>
-          <Burger />
+          <Burger ingredients={this.state.ingredients} />
           <div>Build Controls</div>
         </>
       </div>
