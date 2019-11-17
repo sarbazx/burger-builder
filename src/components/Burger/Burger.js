8e4a0dfcaa48ebeb3eb55cdc25f2dd7aa4084cb3
Taskhyn Maksim <tasqynm@gmail.com>
2019-11-17 17:27:27 +0300

    calc ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const tranformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let tranformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => arr.concat(el), []);
 
+  if (tranformedIngredients.length === 0) {
+    tranformedIngredients = <p>Please adding ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
