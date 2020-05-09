ca8674e85f4527840763a468e47f6b5e0d8095a4
Taskhyn Maksim <tasqynm@gmail.com>
2020-05-09 19:32:13 +0300

    fix typo

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  let tranformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
@@ -12,13 +12,13 @@ const burger = props => {
     })
     .reduce((arr, el) => arr.concat(el), []);
 
-  if (tranformedIngredients.length === 0) {
-    tranformedIngredients = <p>Please adding ingredients</p>;
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please adding ingredients</p>;
   }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type='bread-top' />
-      {tranformedIngredients}
+      {transformedIngredients}
       <BurgerIngredient type='bread-bottom' />
     </div>
   );
