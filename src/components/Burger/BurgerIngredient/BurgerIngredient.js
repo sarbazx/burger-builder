0e441deb54eeb74c141ab42fc653b70ea52f3971
Taskhyn Maksim <tasqynm@gmail.com>
2020-05-15 00:00:42 +0300

    fix bug, add prop type

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -45,7 +45,7 @@ class BurgerIngredient extends Component {
 }
 
 BurgerIngredient.propTypes = {
-  type: PropTypes.string.isRequired
+  type: PropTypes.string.isRequired,
 };
 
 export default BurgerIngredient;
