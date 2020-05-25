e8d93a084e1ffc723db32532551592909f763a86
Taskhyn Maksim <tasqynm@gmail.com>
2020-05-25 21:40:41 +0300

    remove old interceptors

--- src/containers/withErrorHandler/withErrorHandler.js
+++ src/containers/withErrorHandler/withErrorHandler.js
@@ -8,16 +8,20 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     };
     componentWillMount() {
-      axios.interceptors.request.use((req) => {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({ error });
         }
       );
+    }
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
@@ -27,7 +31,9 @@ const withErrorHandler = (WrappedComponent, axios) => {
     render() {
       return (
         <>
-          <Modal show={this.state.error} modalClosed={this.errorConfirmedHandler}>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}>
             {this.state.error ? this.state.error.message : null}
           </Modal>
           <WrappedComponent {...this.props} />
