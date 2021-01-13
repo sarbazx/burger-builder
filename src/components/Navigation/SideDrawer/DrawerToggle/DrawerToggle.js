29c4f202bbf363bbb5fe48eded7179f5ada3f4d7
Taskhyn Maksim <tasqynm@gmail.com>
2021-01-13 15:17:25 +0300

    drawer toggle

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./DrawserToggle.module.css";
 
 const drawerToggle = props => (
-  <div class={styles.DrawerToggle} onClick={props.clicked}>
+  <div className={styles.DrawerToggle} onClick={props.clicked}>
     <div />
     <div />
     <div />
